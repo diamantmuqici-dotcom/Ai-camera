2d8241b1cca1812769dab367a6578d8212b0e809
Sharritech <diamantmuqici@gmail.com>
2026-09-19 14:13:13 +0200

    Rework app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 "use strict";
 
-window.PixelCameraApp = Object.freeze({
-  version:"modular-1",
+window.PixelCameraApp=Object.freeze({
+  version:"rework-2026-09",
   serviceWorkerSupported:"serviceWorker" in navigator,
-  install(){
-    if(this.serviceWorkerSupported&&location.protocol!=="file:")
-      return navigator.serviceWorker.register("./service-worker.js").catch(()=>null);
-    return Promise.resolve(null);
+  async install(){
+    if(!this.serviceWorkerSupported||location.protocol==="file:") return null;
+    try{
+      return await navigator.serviceWorker.register("./service-worker.js",{scope:"./"});
+    }catch(error){
+      console.warn("[PixelCameraApp] service worker registration failed",error);
+      return null;
+    }
   }
 });
+
 window.PixelCameraApp.install();
